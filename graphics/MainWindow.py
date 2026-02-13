379f1ab1a8a00b356b3bab935f6fde23fc7f185c
RyanDeBarros <ryan2004db@gmail.com>
2026-02-13 17:03:31 -0500

    Open file

--- graphics/MainWindow.py
+++ graphics/MainWindow.py
@@ -1,5 +1,8 @@
+from pathlib import Path
+
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QMainWindow, QScrollArea
+from PySide6.QtGui import QShortcut, QKeySequence
+from PySide6.QtWidgets import QMainWindow, QScrollArea, QFileDialog
 
 from ui import Ui_MainWindow
 
@@ -23,3 +26,16 @@ class MainWindow(QMainWindow):
 		self.editCaseCapitalize = ToolButton(self.ui.editCaseCapitalize, get_edit_case_action(EditCaseOption.Capitalize))
 		self.editCaseSentence = ToolButton(self.ui.editCaseSentence, get_edit_case_action(EditCaseOption.Sentence))
 		self.editCaseTitle = ToolButton(self.ui.editCaseTitle, get_edit_case_action(EditCaseOption.Title))
+
+		open_shortcut = QShortcut(QKeySequence("Ctrl+O"), self)
+		open_shortcut.activated.connect(self.open_file_prompt)
+
+	def open_file_prompt(self):
+		# TODO store last directory opened
+		filenames, _ = QFileDialog.getOpenFileNames(self, "Open File", "", "Text Files (*.txt, *.md, *.log);; All Files (*)")
+		if filenames:
+			for file in filenames:
+				self.open_file(file)
+
+	def open_file(self, file):
+		self.ui.textArea.setPlainText(Path(file).read_text())
